50a44a55e60f18c0c119078af40b9b7425e6ffbb
James Ullom <jamesullom@Jamess-MacBook-Pro-2.local>
2018-09-26 10:28:39 -0600

    First commit

---
+++ splitview-app/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  splitview-app
+//
+//  Created by James Ullom on 9/26/18.
+//  Copyright © 2018 Hammer of the Gods Software. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
