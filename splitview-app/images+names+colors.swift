2c477025e2618cf10c0e2e906a84dc33504dbb17
James Ullom <jamesullom@Jamess-MacBook-Pro-2.local>
2018-09-26 11:18:03 -0600

    Added the assets and setup the cell on the mastercontroller.

---
+++ splitview-app/images+names+colors.swift
@@ -0,0 +1,27 @@
+//
+//  images+names+colors.swift
+//  splitview-app
+//
+//  Created by James Ullom on 9/26/18.
+//  Copyright © 2018 Hammer of the Gods Software. All rights reserved.
+//
+
+import UIKit
+
+// Defining some static data to use in the application
+
+// Images
+let camera = UIImage(named: "1")!
+let city = UIImage(named: "2")!
+let buffalo = UIImage(named: "3")!
+let flowers = UIImage(named: "4")!
+let stand = UIImage(named: "5")!
+let urban = UIImage(named: "6")!
+
+let imageArray = [camera, city, buffalo, flowers, stand, urban]
+
+// Names
+var namesArray = ["CAMERA","CITY","ANIMALS","FLOWERS","STANDS","URBAN"]
+
+// Colors
+let colorsArray = [#colorLiteral(red: 1, green: 0, blue: 0, alpha: 1),#colorLiteral(red: 0.1411764771, green: 0.3960784376, blue: 0.5647059083, alpha: 1),#colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1),#colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1),#colorLiteral(red: 0.1215686277, green: 0.01176470611, blue: 0.4235294163, alpha: 1),#colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)]
